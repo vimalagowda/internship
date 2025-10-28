27993608855bf5da3d3c25692aadc535558de50b
Vimala K.V <vimalagowda25@gmail.com>
2025-10-28 09:39:49 +0100

    changes

--- practicw.py
+++ practicw.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df=pd.read_csv("C:\Users\vimal\Downloads\practice\internship\chemical_compounds.csv")
+df=pd.read_csv("C:/Users/vimal/Downloads/practice/internship/chemical_compounds.csv")
 df.head()
